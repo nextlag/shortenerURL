cd0c3dc139478a15c034a0ce444db44c2e705612
nextbug <nextbug@ya.ru>
2023-10-04 08:00:42 +0300

    edit main.go, main_test.go, postHandler

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -20,7 +20,7 @@ func main() {
 	router.Get("/{id}", handlers.GetHandler(db))
 
 	// Создаем маршрут для обработки POST запросов
-	router.Post("/", handlers.Route(db))
+	router.Post("/", handlers.PostHandler(db))
 
 	log.Printf("Start server: %s | ShortenerURL: %s", *config.Address, *config.URLShort)
 	log.Fatal(http.ListenAndServe(*config.Address, router))
