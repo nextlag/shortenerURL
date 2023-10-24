12ca80cb86450dbcd6b4d2e08019990663ab03dc
nextbug <nextbug@ya.ru>
2023-10-25 02:01:55 +0300

    fix

--- internal/handlers/httpserver/apiSave.go
+++ internal/handlers/httpserver/apiSave.go
@@ -27,51 +27,43 @@ const aliasLength = 8
 
 func Shorten(log *zap.Logger, db storage.Storage) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		contentType := r.Header.Get("Content-Type")
-
-		// Проверяем, что Content-Type равен "application/json"
-		if contentType != "application/json" {
-			http.Error(w, "expected Content-Type: application/json", http.StatusBadRequest)
+		var req Request
+		err := render.DecodeJSON(r.Body, &req)
+		if errors.Is(err, io.EOF) {
+			// Такую ошибку встретим, если получили запрос с пустым телом.
+			log.Error("request body is empty")
+			render.JSON(w, r, resp.Error("empty request"))
 			return
-		} else {
-			var req Request
-			err := render.DecodeJSON(r.Body, &req)
-			if errors.Is(err, io.EOF) {
-				// Такую ошибку встретим, если получили запрос с пустым телом.
-				log.Error("request body is empty")
-				render.JSON(w, r, resp.Error("empty request"))
-				return
-			}
-			if err != nil {
-				log.Error("failed to decode request body")
-				render.JSON(w, r, resp.Error("failed to decode request"))
-				return
-			}
-
-			log.Info("request body decoded", zap.Any("request", req))
-			if err := validator.New().Struct(req); err != nil {
-				var validateErr validator.ValidationErrors
-				errors.As(err, &validateErr)
-				log.Error("invalid request")
-				render.JSON(w, r, resp.ValidationError(validateErr))
-				return
-			}
-
-			alias := req.Alias
-			if alias == "" {
-				alias = generatestring.NewRandomString(aliasLength)
-			}
-
-			id := db.Put(alias, req.URL)
-			if id != nil {
-				log.Error("failed to add url")
-				render.JSON(w, r, resp.Error("failed to add url"))
-				return
-			}
-			responseCreated(w, alias)
+		}
+		if err != nil {
+			log.Error("failed to decode request body")
+			render.JSON(w, r, resp.Error("failed to decode request"))
+			return
 		}
 
+		log.Info("request body decoded", zap.Any("request", req))
+		if err := validator.New().Struct(req); err != nil {
+			var validateErr validator.ValidationErrors
+			errors.As(err, &validateErr)
+			log.Error("invalid request")
+			render.JSON(w, r, resp.ValidationError(validateErr))
+			return
+		}
+
+		alias := req.Alias
+		if alias == "" {
+			alias = generatestring.NewRandomString(aliasLength)
+		}
+
+		id := db.Put(alias, req.URL)
+		if id != nil {
+			log.Error("failed to add url")
+			render.JSON(w, r, resp.Error("failed to add url"))
+			return
+		}
+		responseCreated(w, alias)
 	}
+
 }
 
 func responseCreated(w http.ResponseWriter, alias string) {
