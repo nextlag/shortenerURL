b7511137a685019d2ade88e66ea7cdabb3417bd5
nextbug <nextbug@ya.ru>
2023-10-30 22:39:12 +0300

    fix

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,14 +3,13 @@ package config
 import (
 	"flag"
 	"github.com/caarlos0/env/v6"
-	"os"
 )
 
 // ArgsHTTP структура для хранения конфигурации HTTP-сервера.
 type ArgsHTTP struct {
 	Address     string `env:"SERVER_ADDRESS" envDefault:":8080"`
 	URLShort    string `env:"BASE_URL" envDefault:"http://localhost:8080"`
-	FileStorage string
+	FileStorage string `env:"FILE_STORAGE_PATH" envDefault:"/tmp/short-url-db.json"`
 }
 
 // Args - переменная с конфигурацией
@@ -21,12 +20,7 @@ func InitializeArgs() error {
 	// Определение флагов командной строки для настройки конфигурации.
 	flag.StringVar(&Args.Address, "a", Args.Address, "Address HTTP-server")
 	flag.StringVar(&Args.URLShort, "b", Args.URLShort, "Base URL")
-	flag.StringVar(&Args.FileStorage, "f", "short-url-db.json", "file to save links")
-	flag.Parse()
-
-	if fileStorage := os.Getenv("FILE_STORAGE_PATH"); fileStorage != "" {
-		Args.FileStorage = fileStorage
-	}
+	flag.StringVar(&Args.FileStorage, "f", Args.FileStorage, "Storage in file.json")
 
 	// Считывание значений флагов командной строки и переменных окружения в структуру Args.
 	if err := env.Parse(&Args); err != nil {
