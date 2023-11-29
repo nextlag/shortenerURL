9f7eaafe9ecfde518e50c09719f9cf1bab24187a
nextbug <nextbug@ya.ru>
2023-11-29 20:15:29 +0300

    fix

--- internal/transport/rest/handlers/save.go
+++ internal/transport/rest/handlers/save.go
@@ -18,6 +18,7 @@ import (
 func Save(db app.Storage) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		log := lg.New()
+
 		// Считываем тело запроса (оригинальный URL)
 		body, err := io.ReadAll(r.Body)
 		if err != nil {
@@ -25,18 +26,20 @@ func Save(db app.Storage) http.HandlerFunc {
 			return
 		}
 
-		//  Попытка сохранить short-URL и оригинальный URL в хранилище
+		// Попытка сохранить short-URL и оригинальный URL в хранилище
 		alias, err := db.Put(r.Context(), string(body))
-		// обработка конфликта дубликатов
+
+		// Обработка конфликта дубликатов
 		if errors.Is(err, dbstorage.ErrConflict) {
 			log.Error("duplicate url", zap.String("alias", alias), zap.String("url", string(body)))
-			w.WriteHeader(http.StatusConflict)
-			_, err = fmt.Fprintf(w, "%s/%s", config.Config.URLShort, alias)
+			http.Error(w, fmt.Sprintf("%s/%s", config.Config.URLShort, alias), http.StatusConflict)
 			return
 		}
+
+		// Обработка других ошибок
 		if err != nil {
-			er := fmt.Sprintf("failed to add URL: %s", err)
-			http.Error(w, er, http.StatusInternalServerError)
+			log.Error("failed to add URL", zap.Error(err))
+			http.Error(w, fmt.Sprintf("failed to add URL: %s", err), http.StatusInternalServerError)
 			return
 		}
 
