7c77c55ebbfad701d1cd5d0550fadf26f7c4dd64
nextbug <nextbug@ya.ru>
2023-12-12 20:40:54 +0300

    fix

--- internal/storage/storage_test.go
+++ internal/storage/storage_test.go
@@ -4,15 +4,18 @@ import (
 	"os"
 	"testing"
 
+	"go.uber.org/zap"
+
 	"github.com/nextlag/shortenerURL/internal/storage"
 	"github.com/nextlag/shortenerURL/internal/usecase"
 )
 
 func TestSettings(t *testing.T) {
-	fname := "file_test.json"
-	defer os.Remove(fname)
+	fileStorage := "file_test.json"
+	log := zap.NewNop()
+	defer os.Remove(fileStorage)
 	data := usecase.NewRequest("-", "12345", "https://yandex.ru")
-	if err := storage.Save(fname, data.GetEntityRequest().Alias, data.GetEntityRequest().URL); err != nil {
+	if err := storage.Save(log, fileStorage, data.GetEntityRequest().Alias, data.GetEntityRequest().URL); err != nil {
 		t.Error(err)
 	}
 }
