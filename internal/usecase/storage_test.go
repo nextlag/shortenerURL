1e6fa3a355a2b7d8eb7e47412188539fc37e5d51
nextbug <nextbug@ya.ru>
2024-07-22 23:19:20 +0300

    fix'

--- internal/usecase/storage_test.go
+++ internal/usecase/storage_test.go
@@ -7,11 +7,9 @@ import (
 
 func TestSettings(t *testing.T) {
 	fileStorage := "file_test.json"
-	fileDel := "file_del.json"
-	defer os.Remove(fileDel)
 	defer os.Remove(fileStorage)
 	data := NewFileStorage("1", "12345", "http://yandex.ru")
-	if err := save(fileStorage, fileDel, data.Alias, data.URL, 1, false); err != nil {
+	if err := save(fileStorage, data.Alias, data.URL, 1, false); err != nil {
 		t.Error(err)
 	}
 }
